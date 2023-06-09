bea7b2b086998baef1df90ea552ccde558304e0d
Winta38 <winta38@gmail.com>
2023-06-10 03:12:22 +0500

    Задание ред.

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,19 +1,19 @@
-const hasTooltip = document.getElementsByClassName("has-tooltip"); //список ссылок
+const hasTooltip = document.getElementsByClassName("has-tooltip");
 
 for (const item of hasTooltip) {
-  const tooltip = document.createElement("div"); //создаем div
-  tooltip.classList.add("tooltip"); //присваиваем ему класс
-  tooltip.textContent = item.getAttribute("title"); //содержание
-  tooltip.setAttribute("style", "top: 0; left: 0"); //атрибуты
+  const tooltip = document.createElement("div");
+  tooltip.classList.add("tooltip");
+  tooltip.textContent = item.getAttribute("title");
+  tooltip.setAttribute("style", "top: 0; left: 0");
 
-  item.appendChild(tooltip); //добавляем div
+  item.appendChild(tooltip);
 
   item.onclick = () => {
-    const { top, left } = item.getBoundingClientRect(); //находим координаты ссылки
-    tooltip.style.top = top - 30 + "px"; //присваиваем кординаты подсказке
+    const { top, left } = item.getBoundingClientRect();
+    tooltip.style.top = top - 30 + "px";
     tooltip.style.left = left + "px";
 
-    item.querySelector(".tooltip").classList.toggle("tooltip_active"); //активируем подсказку
+    item.querySelector(".tooltip").classList.toggle("tooltip_active");
     return false;
   };
 }
